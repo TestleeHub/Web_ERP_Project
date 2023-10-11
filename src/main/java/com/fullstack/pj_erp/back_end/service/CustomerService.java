0519077800b884600d3f95cb34a28d97400bfb07
DESKTOP-F2MG5OT\ICT02-03 <swo7471@naver.com>
2023-10-11 17:13:27 +0900

    거래처 목록 페이지, 거래처 입력 팝업 페이지. 거래처 목록 및 거래내역 DTO. 컨트롤러 시작

---
+++ src/main/java/com/fullstack/pj_erp/back_end/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.fullstack.pj_erp.back_end.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class CustomerService {
+
+}
